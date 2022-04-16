6e97896a26ff70cd76da00776803c09569a48e1a
Lamaab02 <69766789+Lamaab02@users.noreply.github.com>
2022-04-16 14:40:42 -0400

    test

--- server.js
+++ server.js
@@ -1,77 +1,81 @@
 const express = require('express')
+const { get } = require('http')
 const app = express()
 
-var port =5000
+const args = require('minimist')(process.argv.slice(2))
+args["port"]
 
-const server = app.listen(port, () =>{
-    console.log('App is running on port %PORT%'.replace('%PORT%', port))
+const port = args.port || process.env.port || 5000
+
+const server = app.listen(port, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%',port))
 });
 
-
 function coinFlip() {
-    return Math.random() > 0.5 ? 'heads' : 'tails'
+    const result = (0 == Math.round(Math.random()) ? "heads" : "tails")
+    return result
 }
 
 function coinFlips(flips) {
-    const result = []
-    for(let i = 0; i < flips; i++){
+    let result = []
+    for (let i = 0; i < flips; i++) {
       result[i] = coinFlip()
     }
     return result
 }
 
 function countFlips(array) {
-    let head = 0
-    let tail = 0
-    array.forEach(element => {
-      if(element == "heads") head++
-      else tail++
-    });
-    if (head == 0) {
-      return {'tails': tail}
-    } else if (tail == 0) {
-      return {'heads': head}
-    } else {
-      return {'heads': head, 'tails': tail}
+    const result = {
+      heads: 0,
+      tails: 0,
     }
+    let res = [0, 0]
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] == "heads") {
+        result.heads++
+      } else {
+        result.tails++
+      }
+    }
+    return result
 }
-
 
 function flipACoin(call) {
-    let flip = coinFlip()
-    const result = ""
-    if(call == flip) return {call: call, flip: flip, result: 'win'}
-    else return {call: call, flip: flip, result: 'lose'}
-}
+    const result = {
+      call: call
+    }
+    result.flip = coinFlip()
+    result.result = call == result.flip ? "win" : "lose"
+    return result;
+  }
 
+app.get('/app/', (req, res) => {
+    // res.status(200).end('OK')
+    // Respond with status 200
+        res.statusCode = 200;
+    // Respond with status message "OK"
+        res.statusMessage = 'OK';
+        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+        res.end(res.statusCode+ ' ' +res.statusMessage)
+    });
 
-
-
-app.get('/app/', (req, res)=>{
-    res.statusCode = 200;
-    res.statusMessage = "OK"
-    res.writeHead(res.statusCode, {'Content-Type' : 'text/plain'});
-    res.end(res.statusCode+ ' ' +res.statusMessage)
+app.get('/app/flip/', (req, res) => {
+    res.status(200).json({ 'flip' : coinFlip() })
 });
 
-app.get('/app/flip/', (req, res)=>{
-    res.status(200).json({'flip' : coinFlip()})
-});
+app.get('/app/flip/call/heads', (req, res) => {
+    res.status(200).send(flipACoin('heads'))
+})
+
+app.get('/app/flip/call/tails', (req, res) => {
+    res.status(200).send(flipACoin('tails'))
+})
 
 app.get('/app/flips/:number', (req, res) => {
-    const flips = coinFlips(req.params.number)
-    res.status(200).json({'raw' : flips, 'summary' : countFlips(flips)})
-});
-
-app.get('/app/flip/call/heads', (req, res) =>{
-    res.status(200).send(flipACoin('heads'))
-});
-
-app.get('/app/flip/call/tails', (req, res) =>{
-    res.status(200).send(flipACoin('tails'))
-});
+    const arr = coinFlips(req.params.number)
+    res.status(200).json({ 'raw' : arr, 'summary' : countFlips(arr) })
+})
 
 app.use(function(req, res){
-    res.status(404).send("404 NOT fOUND")
-    res.type("text/plain")
-})
+    res.status(404).send('404 NOT FOUND')
+});
